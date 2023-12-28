9caceb5793117d6c7200a549bc40347928786997
Luis Ceballos <ceballos97@gmail.com>
2023-12-27 18:37:40 -0800

    5-1 Assignment

---
+++ Chapter_5/TIY_5-1.js
@@ -0,0 +1,8 @@
+// function returns 'Hello + name'
+function sayHello(name) {
+    return `Hello, ${name}!`;
+}
+
+//prints funtion and also converts the output to all uppercase
+console.log(sayHello('Louie').toUpperCase());
+
