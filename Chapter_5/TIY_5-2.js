d773cbfd6cf6eb1a0e57b499e5b39876d8cf878a
Luis Ceballos <ceballos97@gmail.com>
2023-12-28 17:16:23 -0800

    started tiy 5-2

---
+++ Chapter_5/TIY_5-2.js
@@ -0,0 +1,25 @@
+/* A function that takes a number from 0 to 5 and returns the ­ English
+word for that number. For example, 1 should return "one". Hint: use an
+array to define the mapping from numbers to strings. */
+
+function numberConversion() {
+
+}
+
+
+/* A function with no ­ parameters that prints how many times it’s been
+called. Hint: define a variable outside of the function to keep track of the
+number of calls, like we did in the “Side Effects” section on page 77. */
+
+function functionCounter() {
+
+}
+
+
+
+/* A function that prints the current date and time. Hint: you can get the
+current date and time with new Date(). */
+
+function dateTime() {
+
+}
