fe72572f21a3a4a4e179246f5e42cb124abafaef
Luis Ceballos <ceballos97@gmail.com>
2023-12-30 20:45:59 -0800

    Finished first part

--- Chapter_5/TIY_5-2.js
+++ Chapter_5/TIY_5-2.js
@@ -2,14 +2,67 @@
 word for that number. For example, 1 should return "one". Hint: use an
 array to define the mapping from numbers to strings. */
 
-function numberConversion() {
-
+function numberConversion(n) {
+    if (n === 0) {
+        console.log('Zero');
+    } else if (n === 1) {
+        console.log('One');
+    } else if (n === 2) {
+        console.log('Two');
+    } else if (n === 3) {
+        console.log('Three');
+    } else if (n === 4) {
+        console.log('Four');
+    } else if (n === 5) {
+        console.log('Five');
+    }
 }
+
+//----------------------------------------------------------------
+
+let validInputs = [0, 1, 2, 3, 4, 5];
+let returnValues = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five'];
+
+let numberConversion2 = function(n) {
+    if (n === n[0]) {
+        return returnValues[0];
+    } else if (n === n[1]) {
+        return returnValues[1];
+    } else if (n === n[2]) {
+        return returnValues[2];
+    } else if (n === n[3]) {
+        return returnValues[3];
+    } else if (n === n[4]) {
+        return returnValues[4];
+    } else if (n === n[5]) {
+        return returnValues[5];
+    }
+};
+
+//----------------------------------------------------------------
+
+const numberConversion3 = n => {
+    if (n === n[0]) {
+        return returnValues[0];
+    } else if (n === n[1]) {
+        return returnValues[1];
+    } else if (n === n[2]) {
+        return returnValues[2];
+    } else if (n === n[3]) {
+        return returnValues[3];
+    } else if (n === n[4]) {
+        return returnValues[4];
+    } else if (n === n[5]) {
+        return returnValues[5];
+    }
+};
+
 
 
 /* A function with no ­ parameters that prints how many times it’s been
 called. Hint: define a variable outside of the function to keep track of the
 number of calls, like we did in the “Side Effects” section on page 77. */
+
 
 function functionCounter() {
 
@@ -17,8 +70,10 @@ function functionCounter() {
 
 
 
+
 /* A function that prints the current date and time. Hint: you can get the
 current date and time with new Date(). */
+
 
 function dateTime() {
 
